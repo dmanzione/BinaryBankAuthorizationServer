18255ef9ab5c3261296e3a111bd14643cc03597f
Donato Manzione <donato.manzione@smoothstack.com>
2023-06-01 02:47:04 -0400

    first commit

---
+++ src/main/java/com/binary_beasts/binary_banking/auth_server/ServerApplication.java
@@ -0,0 +1,12 @@
+package com.binary_beasts.binary_banking.auth_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
